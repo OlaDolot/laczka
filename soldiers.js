89d044243095926036c884fb7a4dacbd1a22874a
oladolot <ola.dolot@gmail.com>
2016-02-14 17:49:29 +0100

    First version of website

---
+++ soldiers.js
@@ -0,0 +1,327 @@
+/**
+ * Created by ola on 13.02.16.
+ */
+
+var osoby = {
+        'S_Abramowski' : {
+            'img': "img/zolnierze/s_abramowski.jpg",
+            'rnk': "plutonowy",
+            'who': "Stanisław Abramowski",
+            'psd': "ps. BURY",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Stanis%C5%82aw_Abramowski"
+        },
+        'W_Borowiec' : {
+            'img': "img/zolnierze/w_borowiec.jpg",
+            'rnk': "żołnierz",
+            'who': "Władysław Borowiec",
+            'psd': "ps. ŻBIK",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/W%C5%82adys%C5%82aw_Borowiec"
+        },
+        'H_Borowski' : {
+            'img': "img/zolnierze/h_borowski.jpg",
+            'rnk': "porucznik",
+            'who': "Henryk Borowski",
+            'psd': "ps. TRZMIEL",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Henryk_Borowski_%28%C5%BCo%C5%82nierz_AK%29"
+        },
+        'B_Budelewski' : {
+            'img': "img/zolnierze/b_budelewski.jpg",
+            'rnk': "żołnierz",
+            'who': "Bolesław Budelewski",
+            'psd': "ps. PŁUG",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Boles%C5%82aw_Budelewski"
+        },
+        'E_Bukowski' : {
+            'img': "img/zolnierze/e_bukowski.jpg",
+            'rnk': "żołnierz",
+            'who': "Edmund Bukowski",
+            'psd': "ps. EDMUND",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Edmund_Bukowski"
+        },
+        'J_Czeredys' : {
+            'img': "img/zolnierze/j_czeredys.jpg",
+            'rnk': "major",
+            'who': "Jan Czeredys",
+            'psd': "",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Jan_Czeredys"
+        },
+        'J_Czerwiakowski' : {
+            'img': "img/zolnierze/j_czerwiakowski.jpg",
+            'rnk': "oficer NSZ",
+            'who': "Julian Czerwiakowski",
+            'psd': "ps. JERZY TARNOWSKI",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Julian_Czerwiakowski"
+        },
+        'B_Czescik' : {
+            'img': "img/zolnierze/b_czescik.jpg",
+            'rnk': "żołnierz",
+            'who': "Bolesław Częścik",
+            'psd': "ps. ORLIK",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Boles%C5%82aw_Cz%C4%99%C5%9Bcik"
+        },
+        'H_Dekutowski' : {
+            'img': "img/zolnierze/h_dekutowski.jpg",
+            'rnk': "major",
+            'who': "Hieronim Dekutowski",
+            'psd': "ps. ZAPORA",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Hieronim_Dekutowski"
+        },
+        'A_Gajdek' : {
+            'img': "img/zolnierze/a_gajdek.jpg",
+            'rnk': "podoficer",
+            'who': "Adam Gajtek",
+            'psd': "ps. AGATA",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Adam_Gajdek"
+        },
+        'S_Glowacki' : {
+            'img': "img/zolnierze/s_glowacki.jpg",
+            'rnk': "żołnierz",
+            'who': "Stefan Głowacki",
+            'psd': "ps. SMUGA",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Stefan_G%C5%82owacki"
+        },
+        'R_Gronski' : {
+            'img': "img/zolnierze/r_gronski.jpg",
+            'rnk': "porucznik",
+            'who': "Roman Groński",
+            'psd': "ps. ŻBIK",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Roman_Gro%C5%84ski"
+        },
+        'M_Kaczmarek' : {
+            'img': "img/zolnierze/m_kaczmarek.jpg",
+            'rnk': "kurier",
+            'who': "Marian Kaczmarek",
+            'psd': "ps. PAWEŁ",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Marian_Kaczmarek_%28kurier%29"
+        },
+        'S_Kasznica' : {
+            'img': "img/zolnierze/s_kasznica.jpg",
+            'rnk': "komendant NSZ",
+            'who': "Stanisław Kasznica",
+            'psd': "ps. WĄSACZ",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Stanis%C5%82aw_Kasznica_%28komendant_NSZ%29"
+        },
+        'A_Kita' : {
+            'img': "img/zolnierze/a_kita.jpg",
+            'rnk': "pułkownik",
+            'who': "Aleksander Kita",
+            'psd': "",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Aleksander_Kita"
+        },
+        'B_Kontrym' : {
+            'img': "img/zolnierze/b_kontrym.jpg",
+            'rnk': "major",
+            'who': "Bolesław Kontrym",
+            'psd': "ps. ŻMUDZIN, BIAŁY",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Boles%C5%82aw_Kontrym"
+        },
+        'J_Kozlowski' : {
+            'img': "img/zolnierze/j_kozlowski.jpg",
+            'rnk': "komendant",
+            'who': "Józef Kozłowski",
+            'psd': "ps. LAS",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/J%C3%B3zef_Koz%C5%82owski_%281910-1949%29"
+        },
+        'Z_Kulinski' : {
+            'img': "img/zolnierze/z_kulinski.jpg",
+            'rnk': "żołnierz",
+            'who': "Zygrfyd Kuliński",
+            'psd': "ps. ALBIN",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Zygfryd_Kuli%C5%84ski"
+        },
+        'S_Kutryb' : {
+            'img': "img/zolnierze/s_kutryb.jpg",
+            'rnk': "żołnierz",
+            'who': "Stanisław Kutryb",
+            'psd': "ps. RYŚ, REKIN",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Stanis%C5%82aw_Kutryb"
+        },
+        'S_Lukasik' : {
+            'img': "img/zolnierze/s_lukasik.jpg",
+            'rnk': "kapitan",
+            'who': "Stanisław Łukasik",
+            'psd': "ps. RYŚ, RYSZARD",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Stanis%C5%82aw_%C5%81ukasik"
+        },
+        'J_Lukaszewicz' : {
+            'img': "img/zolnierze/j_lukaszewicz.jpg",
+            'rnk': "sierżant",
+            'who': "Józef Łukaszewicz",
+            'psd': "ps. WALEK, KRUK",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/J%C3%B3zef_%C5%81ukaszewicz_%28%C5%BCo%C5%82nierz_NSZ%29"
+        },
+        'J_Miatkowski' : {
+            'img': "img/zolnierze/j_miatkowski.jpg",
+            'rnk': "porucznik",
+            'who': "Jerzy Miatkowski",
+            'psd': "ps. ZAWADA",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Jerzy_Miatkowski"
+        },
+        'S_Mieszkowski' : {
+            'img': "img/zolnierze/k_mieszk.png",
+            'rnk': "komandor",
+            'who': "Stanisław Mieszkowski",
+            'psd': "",
+            'bio': " Tu został znaleziony Komandor Stanisław Mieszkowski. W 1949 mianowany dowódcą Floty Marynarki Wojennej." +
+                   " Aresztowany w 1950 roku, dwa lata spędził w więzieniu. Rozstrzelany w 1952.",
+            'url': "https://pl.wikipedia.org/wiki/Stanis%C5%82aw_Mieszkowski"
+        },
+        'A_Olechnowicz' : {
+            'img': "img/zolnierze/a_olechnowicz.jpg",
+            'rnk': "podpułkownik",
+            'who': "Antoni Olechnowicz",
+            'psd': "ps. METEOR",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Antoni_Olechnowicz"
+        },
+        'M_Orlik' : {
+            'img': "img/zolnierze/m_orlik.jpg",
+            'rnk': "pułkownik",
+            'who': "Marian Orlik",
+            'psd': "",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Marian_Orlik"
+        },
+        'R_Pawlowski' : {
+            'img': "img/zolnierze/r_pawlowski.jpg",
+            'rnk': "podchorąży",
+            'who': "Robert Henryk Pawłowski",
+            'psd': "ps. HENRYK ORŁOWSKI",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Roman_Henryk_Paw%C5%82owski"
+        },
+        'T_Pelak' : {
+            'img': "img/zolnierze/t_pelak.jpg",
+            'rnk': "porucznik",
+            'who': "Tadeusz Pelak",
+            'psd': "ps. JUNAK",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Tadeusz_Pelak"
+        },
+        'Z_Przybyszewski' : {
+            'img': "img/zolnierze/z_przybyszewski.jpg",
+            'rnk': "komandor porucznik",
+            'who': "Zbigniew Przybyszewski",
+            'psd': "",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Zbigniew_Przybyszewski"
+        },
+        'E_Pytko' : {
+            'img': "img/zolnierze/e_pytko.jpg",
+            'rnk': "podporucznik pilot",
+            'who': "Edward Pytko",
+            'psd': "",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Edward_Pytko"
+        },
+        'K_Rakoczy' : {
+            'img': "img/zolnierze/k_rakoczy.jpg",
+            'rnk': "żołnierz",
+            'who': "Karol Rakoczy",
+            'psd': "ps. BYSTRY",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Karol_Rakoczy"
+        },
+        'E_Smolinski' : {
+            'img': "img/zolnierze/e_smolinski.jpg",
+            'rnk': "żołnierz AK",
+            'who': "Eugeniusz Smoliński",
+            'psd': "ps. KAZIMIERZ STANISZEWSKI",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Eugeniusz_Smoli%C5%84ski"
+        },
+        'D_Sosnowski' : {
+            'img': "img/zolnierze/d_sosnowski.jpg",
+            'rnk': "kapral podchorąży",
+            'who': "Dionizy Sosnowski",
+            'psd': "pa. ZBIGNIEW",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Dionizy_Sosnowski"
+        },
+        'Z_Szendzielarz' : {
+            'img': "img/zolnierze/z_szendzielarz.jpg",
+            'rnk': "major",
+            'who': "Zygmunt Szendzielarz",
+            'psd': "ps. ŁUPASZKA",
+            'bio': "Tu został znaleziony Zygmunt Szendzielarz pseudonim Łupaszka.",
+            'url': "https://pl.wikipedia.org/wiki/Zygmunt_Szendzielarz"
+        },
+        'Z_Szymanowski' : {
+            'img': "img/zolnierze/z_szymanowski.jpg",
+            'rnk': "porucznik",
+            'who': "Zygmunt Szymanowski",
+            'psd': "ps. JEZIERZA",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Zygmunt_Szymanowski"
+        },
+        'L_Swider' : {
+            'img': "img/zolnierze/l_swider.jpg",
+            'rnk': "major",
+            'who': "Ludwik Świder",
+            'psd': "",
+            'bio': "",
+            'url': "https://pl.wikipedia.org/wiki/Ludwik_%C5%9Awider"
+        },
+        'A_Tomaszewski' : {
+            'img': "img/zolnierze/a_tomaszewski.jpg",
+            'rnk': "kapitan",
+            'who': "Aleksander Tomaszewski",
+            'psd': "ps. AL, BOŃCZA",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Aleksander_Tomaszewski_%28kapitan%29"
+        },
+        'E_Tudruj' : {
+            'img': "img/zolnierze/e_tudruj.jpg",
+            'rnk': "porucznik",
+            'who': "Edmund Turduj",
+            'psd': "ps. MUNDEK",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Edmund_Tudruj"
+        },
+        'W_Walicki' : {
+            'img': "img/zolnierze/w_walicki.jpg",
+            'rnk': "porucznik",
+            'who': "Wacław Walicki",
+            'psd': "ps. 111, PAN MICHAŁ",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Wac%C5%82aw_Walicki"
+        },
+        'A_Wasilewski' : {
+            'img': "img/zolnierze/a_wasilewski.jpg",
+            'rnk': "porucznik",
+            'who': "Arkadiusz Wasilewski",
+            'psd': "ps. BIAŁY",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Arkadiusz_Wasilewski"
+        },
+        'R_Widelski' : {
+            'img': "img/zolnierze/r_widelski.jpg",
+            'rnk': "żołnierz AK i WIN",
+            'who': "Ryszard Wideslki",
+            'psd': "ps. IRYDION, WIARA",
+            'bio': " ",
+            'url': "https://pl.wikipedia.org/wiki/Ryszard_Widelski"
+        }
+    };
